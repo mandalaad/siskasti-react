9b29340332a2c2260c197039e41fe509afebfa97
muhamadra146 <muhamadra146@gmail.com>
2023-05-19 22:12:12 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Login from './Pages/Login';
 import Sidebar from './Component/Sidebar';
 import Dashboardutama from './Pages/Dashboard-utama/index';
 import Penerimaan from './Pages/penerimaan/Penerimaan';
+import Pengeluaran from './Pages/pengeluaran/Pengeluaran';
+import Laporan from './Pages/laporan/Laporan';
+import Manajemen from './Pages/manajemen-user/Manajemen';
+import Ubahpassword from './Pages/ubah-password/Ubahpassword';
+
 
 function App() {
   return (
@@ -14,6 +19,10 @@ function App() {
       <Route path='/' Component={Dashboardutama}/>
       <Route path='/dashboard' Component={Dashboardutama}/>
       <Route path='/penerimaan' Component={Penerimaan}/>
+      <Route path='/pengeluaran' Component={Pengeluaran}/>
+      <Route path='/laporan' Component={Laporan}/>
+      <Route path='/manajemen-user' Component={Manajemen}/>
+      <Route path='/ubah-password' Component={Ubahpassword}/>
     </Routes>
     </>
   );
