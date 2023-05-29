fe560331cdc3b94756d49a050f5baffc30685394
mandalaad <mandalacs18@gmail.com>
2023-05-29 12:58:09 +0700

    update sidebar

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { AiOutlineCloudUpload, AiOutlineDownload, AiOutlineSetting, AiOutlineDashboard} from 'react-icons/ai'
-import {GoReport} from 'react-icons/go'
+import {AiOutlineSetting, AiOutlineDashboard} from 'react-icons/ai'
+import {AiOutlineShoppingCart, AiFillCaretDown, AiOutlineHistory} from 'react-icons/ai'
 import {HiOutlineLogin} from 'react-icons/hi'
 import {HiOutlineLogout} from 'react-icons/hi'
 import {MdManageAccounts} from 'react-icons/md'
-import {CgFileDocument} from 'react-icons/cg'
+import {MdPayment} from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import '../Sidebar/sidebarstyle.css'
 import Button from 'react-bootstrap/Button';
@@ -12,7 +12,7 @@ import Collapse from 'react-bootstrap/Collapse';
 
 
 const style1 = { color: "white", fontSize: "1.5em" }
-
+const style2 = { color: "white", fontSize: "1.2em" }
 function Sidebar() {
   const [open, setOpen] = useState(false);
   return (
@@ -29,23 +29,13 @@ function Sidebar() {
                     </div>
                 </div>
             </Link>
-            <Link to="/penerimaan" className='sidebar-link'>
-              <div className='menu-sidebar d-flex'>
-                  <div className='logo'>
-                    <i><AiOutlineDownload style={style1}/></i>
-                  </div>
-                  <div className='menu1'>
-                    <Link to="/penerimaan" className='link-menu'>Penerimaan</Link>
-                  </div>
-              </div>
-            </Link>
             <Link to="/pengeluaran" className='sidebar-link'>
               <div className='menu-sidebar d-flex'>
                 <div className='logo'>
-                  <i><AiOutlineCloudUpload style={style1}/></i>
+                  <i><MdPayment style={style1}/></i>
                 </div>
                 <div className='menu1'>
-                  <Link to="/pengeluaran" className='link-menu'>Pengeluaran</Link>
+                  <Link to="/pengeluaran" className='link-menu'>Pembayaran</Link>
                 </div>
               </div>
             </Link>
@@ -57,7 +47,9 @@ function Sidebar() {
                   onClick={() => setOpen(!open)}
                   aria-controls="example-collapse-text"
                   aria-expanded={open}
-                > <i><GoReport style={style1}/></i> Laporan
+                > <i><AiOutlineShoppingCart style={style1}/></i> Transaction <i>
+                  <AiFillCaretDown className='mx-2' style={style2}/>
+                </i>
                 </Button>
                 <Collapse in={open}>
                   <div id="example-collapse-text">
@@ -77,17 +69,19 @@ function Sidebar() {
                         <Link to="/laporan-pengeluaran-mingguan" className='link-menu'>Pengeluaran</Link>
                       </div>
                     </div>
-                    <div className='menu-sidebar d-flex'>
-                      <div className='logo'>
-                        <i><CgFileDocument style={style1}/></i>
-                      </div>
-                      <div className='menu1'>
-                        <Link to="/laporan" className='link-menu'>Rekapitulasi</Link>
-                      </div>
-                    </div>
                   </div>
                 </Collapse>
               </>
+            </Link>
+            <Link to="/pengeluaran" className='sidebar-link'>
+              <div className='menu-sidebar d-flex'>
+                <div className='logo'>
+                  <i><AiOutlineHistory style={style1}/></i>
+                </div>
+                <div className='menu1'>
+                  <Link to="/history" className='link-menu'>History</Link>
+                </div>
+              </div>
             </Link>
             <Link to="/manajemen-user" className='sidebar-link'>
               <div className='menu-sidebar d-flex'>
