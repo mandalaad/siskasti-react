3fc2fc71e8a53a7577d65064b1c014e974b064db
mandalaad <mandalacs18@gmail.com>
2023-06-11 23:38:38 +0700

    fix history table pages

--- src/Component/history-pengeluaran/Historypengeluaran.jsx
+++ src/Component/history-pengeluaran/Historypengeluaran.jsx
@@ -115,193 +115,99 @@ function Historypengeluaran() {
   //   return filteredData;
   // };
 
-  //cara 2
-
-  const [data, setData] = useState([]);
+  // cara 2
+  const [data, setData] = useState([
+    { id: 1, name: 'John Doe', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-15' },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-15' },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-18'  },
+      { id: 4, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-23'},
+      { id: 5, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-22' },
+      { id: 6, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-19'},
+      { id: 7, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-22' },
+      { id: 8, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-10' },
+      { id: 9, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-11' },
+      { id: 10, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-02-12' },
+      { id: 11, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: '2023-03-17' },
+  ]);
+  const [startDate, setStartDate] = useState(null);
   const [filteredData, setFilteredData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
-    // Ambil data dari API atau sumber data lainnya
-    // const dataFromAPI = [
-    //   { nama: 'John Doe', usia: 25, alamat: 'Jakarta', bulan: 1 },
-    //   { nama: 'Jane Smith', usia: 30, alamat: 'Surabaya', bulan: 2 },
-    //   { nama: 'Bob Johnson', usia: 35, alamat: 'Bandung', bulan: 1 },
-    const dataFromAPI = [
-      { id: 1, name: 'John Doe', tanggal: 25 , kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-01-10'), tanggal:+'date', },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-
-      // { id: 2, name: 'Jane Smith', kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      // { id: 3, name: 'Bob Johnson', kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  }
-    ];
-    // ];
-    setData(dataFromAPI);
-    setFilteredData(dataFromAPI);
-  };
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-    filterDataByMonth(date);
-  };
-
-  const filterDataByMonth = (date) => {
-    const filtered = data.filter(
-      (item) => item.bulan === date.getMonth() + 1 && item.tahun === date.getFullYear()
-    );
-    setFilteredData(filtered);
-  };
 
   const handlePrint = () => {
     const doc = new jsPDF();
-    const tableData = filteredData.map((item) => [item.id, item.name, item.kasmasuk, item.kaskeluar, item.nominal, item.keterangan]);
-
-    doc.autoTable({ head: [['ID', 'Nama', 'Kas Masuk', 'Kas Keluar', 'Nominal', 'keterangan',]], body: tableData });
-    doc.save('tabel.pdf');
+    doc.autoTable({
+      head: [['ID', 'Nama', 'Kas Masuk', 'Kas Keluar', 'Nominal', 'keterangan',]],
+      body: filteredData.map(({ id, name, kasmasuk, kaskeluar, nominal, keterangan }) => [id, name, kasmasuk, kaskeluar, nominal, keterangan]),
+    });
+    doc.save('table.pdf');
   };
-  
+
+  const handleFilterByMonth = (date) => {
+    const selectedMonth = date.getMonth();
+    const filteredByMonth = data.filter((item) => {
+      const itemMonth = new Date(item.date).getMonth();
+      return itemMonth === selectedMonth;
+    });
+    setFilteredData(filteredByMonth);
+  };
+
+  const handleShowAllData = () => {
+    setFilteredData(data);
+  };
+
+  useEffect(() => {
+    handleShowAllData();
+  }, []);
+
   return (
     <>
-    {/* <div className="content">
-        <div className="history">
-        <table>
-            <thead>
-                <tr>
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Nama</th>
-                <th>NIK</th>
-                <th>Grade</th>
-                <th>Nominal</th>
-                <th>Unit Kerja</th>
+      <div className="content">
+            <div className="tabel-income">
+              <h2>Riwayat Pengeluaran</h2>
+              <div className="tanggal-cetak mt-3">
+                <div className="kiri">
+                  <div className="tanggal">
+                    <h5>Pilih Tanggal</h5>
+                    <DatePicker selected={startDate} onChange={handleFilterByMonth} showMonthYearPicker />
+                  </div>
+                  <div className="reset mt-3">
+                    <button className="button-print" onClick={handleShowAllData}>Tampilkan Semua Data</button>
+                  </div>
+                </div>
+                <div className="cetak">
+                    <button className="button-print" onClick={handlePrint}>Print</button>
+                  </div>
+              </div>
+              <Table className="table table-bordered mt-5" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>no</th>
+                    <th>Nama</th>
+                    <th>Tanggal</th>
+                    <th>Kas Masuk</th>
+                    <th>Kas Keluar</th>
+                    <th>Nominal</th>
+                    <th>Keterangan</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredData.map(({ id, name, date, tanggal, kasmasuk, kaskeluar, nominal, keterangan }) => (
+                  <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{date}</td>
+                  <td>{kasmasuk}</td>
+                  <td>{kaskeluar}</td>
+                  <td>{nominal}</td>
+                  <td>{keterangan}</td>
                 </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) => (
-                <tr key={index}>
-                    <td>{item.no}</td>
-                    <td>{item.tanggal}</td>
-                    <td>{item.nama}</td>
-                    <td>{item.nik}</td>
-                    <td>{item.grade}</td>
-                    <td>{item.nominal}</td>
-                    <td>{item.unit}</td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
-        </div>
-    </div> */}
-    {/* <div className="content">
-      <div className="tabel-income">
-        <h3>Riwayat Pengeluaran</h3>
-        <div className="content1 mt-4">
-          <div className="tanggal">
-            <div className="date">
-              <DatePicker
-                selected={selectedMonth}
-                onChange={handleMonthChange}
-                dateFormat="MM/yyyy"
-                showMonthYearPicker
-              />
+                  ))}
+                </tbody>
+              </Table>
             </div>
-          </div>
-          <div className="buton">
-            <button onClick={handlePrint} disabled={!selectedMonth}>print</button>
-          </div>
-        </div>
-        {selectedMonth && (
-          <div className="table-responsive">
-          <Table className="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
-          <thead>
-            <tr>
-              <th>no</th>
-              <th>Nama</th>
-              <th>Tanggal</th>
-              <th>Kas Masuk</th>
-              <th>Kas Keluar</th>
-              <th>Nominal</th>
-              <th>Keterangan</th>
-            </tr>
-          </thead>
-          <tbody>
-            {processData(selectedMonth).map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.tanggal}</td>
-                <td>{item.kasmasuk}</td>
-                <td>{item.kaskeluar}</td>
-                <td>{item.nominal}</td>
-                <td>{item.keterangan}</td>
-
-
-              </tr>
-            ))}
-          </tbody>
-          </Table>
+            
       </div>
-      )}  
-      </div>
-    </div> */}
 
-    {/* cara 2 */}
-
-    <div className="content">
-      <div className="tabel-income">
-      <h3>Riwayat Pengeluaran</h3>
-        <div className="tanggal-cetak mt-3">
-          <div className="tanggal">
-            <DatePicker selected={selectedDate} onChange={handleDateChange} dateFormat="MM/yyyy" showMonthYearPicker />
-          </div>
-          <div className="cetak">
-            <button className="button-print" onClick={handlePrint}>Cetak PDF</button>
-          </div>
-        </div>
-      
-      
-      <Table className="table table-bordered mt-5" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-              <th>no</th>
-              <th>Nama</th>
-              <th>Tanggal</th>
-              <th>Kas Masuk</th>
-              <th>Kas Keluar</th>
-              <th>Nominal</th>
-              <th>Keterangan</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item, index) => (
-            <tr key={index}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.tanggal}</td>
-                <td>{item.kasmasuk}</td>
-                <td>{item.kaskeluar}</td>
-                <td>{item.nominal}</td>
-                <td>{item.keterangan}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      
-      </div>
-      
-    </div>
     </>
   )
 }
