6419e317aba7ef0dbfe15ecc55ef757eec4fd7ed
mandalaad <mandalacs18@gmail.com>
2023-06-11 21:38:52 +0700

    update table

--- src/Component/history-pengeluaran/Historypengeluaran.jsx
+++ src/Component/history-pengeluaran/Historypengeluaran.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Table } from "react-bootstrap"
 import './historypengeluaran.css'
 import DatePicker from 'react-datepicker'
@@ -69,52 +69,108 @@ function Historypengeluaran() {
   // };
   // SAMPE SINI
   
-  const [selectedMonth, setSelectedMonth] = useState(null);
+  // const [selectedMonth, setSelectedMonth] = useState(null);
   
-  const handleMonthChange = (date) => {
-    setSelectedMonth(date);
+  // const handleMonthChange = (date) => {
+  //   setSelectedMonth(date);
+  // };
+
+  // const handlePrint = () => {
+  //   if (selectedMonth) {
+  //     const filteredData = processData(selectedMonth); // Memproses data sesuai bulan yang dipilih
+
+  //     const doc = new jsPDF();
+  //     doc.text(`Data for ${selectedMonth.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}`, 10, 10);
+
+  //     doc.autoTable({
+  //       head: [['ID', 'Nama', 'Kas Masuk', 'Kas Keluar', 'Nominal', 'keterangan',]],
+  //       body: filteredData.map((item) => [item.id, item.name, item.kasmasuk, item.kaskeluar, item.nominal, item.keterangan])
+  //     });
+
+  //     doc.save('data.pdf');
+  //   }
+  // };
+
+  // const processData = (selectedMonth) => {
+  //   // Contoh data tabel
+  //   const data = [
+  //     { id: 1, name: 'John Doe', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-01-10'), tanggal:+'date', },
+  //     { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+  //     { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+  //     { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+  //     { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+  //     { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+  //     { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+  //     { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+  //     { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+  //     { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+  //     { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+  //   ];
+
+  //   const filteredData = data.filter(
+  //     (item) =>
+  //       item.date.getMonth() === selectedMonth.getMonth() &&
+  //       item.date.getFullYear() === selectedMonth.getFullYear()
+  //   );
+  //   return filteredData;
+  // };
+
+  //cara 2
+
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    // Ambil data dari API atau sumber data lainnya
+    // const dataFromAPI = [
+    //   { nama: 'John Doe', usia: 25, alamat: 'Jakarta', bulan: 1 },
+    //   { nama: 'Jane Smith', usia: 30, alamat: 'Surabaya', bulan: 2 },
+    //   { nama: 'Bob Johnson', usia: 35, alamat: 'Bandung', bulan: 1 },
+    const dataFromAPI = [
+      { id: 1, name: 'John Doe', tanggal: 25 , kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-01-10'), tanggal:+'date', },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
+
+      // { id: 2, name: 'Jane Smith', kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
+      // { id: 3, name: 'Bob Johnson', kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  }
+    ];
+    // ];
+    setData(dataFromAPI);
+    setFilteredData(dataFromAPI);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    filterDataByMonth(date);
+  };
+
+  const filterDataByMonth = (date) => {
+    const filtered = data.filter(
+      (item) => item.bulan === date.getMonth() + 1 && item.tahun === date.getFullYear()
+    );
+    setFilteredData(filtered);
   };
 
   const handlePrint = () => {
-    if (selectedMonth) {
-      const filteredData = processData(selectedMonth); // Memproses data sesuai bulan yang dipilih
+    const doc = new jsPDF();
+    const tableData = filteredData.map((item) => [item.id, item.name, item.kasmasuk, item.kaskeluar, item.nominal, item.keterangan]);
 
-      const doc = new jsPDF();
-      doc.text(`Data for ${selectedMonth.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}`, 10, 10);
-
-      doc.autoTable({
-        head: [['ID', 'Nama', 'Kas Masuk', 'Kas Keluar', 'Nominal', 'keterangan',]],
-        body: filteredData.map((item) => [item.id, item.name, item.kasmasuk, item.kaskeluar, item.nominal, item.keterangan])
-      });
-
-      doc.save('data.pdf');
-    }
+    doc.autoTable({ head: [['ID', 'Nama', 'Kas Masuk', 'Kas Keluar', 'Nominal', 'keterangan',]], body: tableData });
+    doc.save('tabel.pdf');
   };
-
-  const processData = (selectedMonth) => {
-    // Contoh data tabel
-    const data = [
-      { id: 1, name: 'John Doe', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-01-10'), tanggal:+'date', },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-      { id: 2, name: 'Jane Smith', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-15'),  },
-      { id: 3, name: 'Bob Johnson', tanggal: 25 ,kasmasuk:'30.000', kaskeluar:'20.000', nominal:'50.000', keterangan:'lunas', date: new Date('2023-02-20'),  },
-    ];
-
-    const filteredData = data.filter(
-      (item) =>
-        item.date.getMonth() === selectedMonth.getMonth() &&
-        item.date.getFullYear() === selectedMonth.getFullYear()
-    );
-    return filteredData;
-  };
-
   
   return (
     <>
@@ -148,7 +204,7 @@ function Historypengeluaran() {
         </table>
         </div>
     </div> */}
-    <div className="content">
+    {/* <div className="content">
       <div className="tabel-income">
         <h3>Riwayat Pengeluaran</h3>
         <div className="content1 mt-4">
@@ -199,6 +255,52 @@ function Historypengeluaran() {
       </div>
       )}  
       </div>
+    </div> */}
+
+    {/* cara 2 */}
+
+    <div className="content">
+      <div className="tabel-income">
+      <h3>Riwayat Pengeluaran</h3>
+        <div className="tanggal-cetak mt-3">
+          <div className="tanggal">
+            <DatePicker selected={selectedDate} onChange={handleDateChange} dateFormat="MM/yyyy" showMonthYearPicker />
+          </div>
+          <div className="cetak">
+            <button className="button-print" onClick={handlePrint}>Cetak PDF</button>
+          </div>
+        </div>
+      
+      
+      <Table className="table table-bordered mt-5" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+              <th>no</th>
+              <th>Nama</th>
+              <th>Tanggal</th>
+              <th>Kas Masuk</th>
+              <th>Kas Keluar</th>
+              <th>Nominal</th>
+              <th>Keterangan</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.tanggal}</td>
+                <td>{item.kasmasuk}</td>
+                <td>{item.kaskeluar}</td>
+                <td>{item.nominal}</td>
+                <td>{item.keterangan}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      
+      </div>
+      
     </div>
     </>
   )
