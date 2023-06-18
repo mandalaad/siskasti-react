6425810f404ebac09f4427c5930b82ad7898274a
mandalaad <mandalacs18@gmail.com>
2023-06-18 14:22:56 +0700

    add dev2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Pembayarankaryawan from './pages/pembayaran-karyawan/Pembayarankaryawan';
 import Profilekaryawan from './pages/profile-karyawan/Profilekaryawan';
 import LaporanKaryawan from './pages/LaporanKaryawan/LaporanKaryawan';
-
+import Dashboarddepartemen from './pages/dashboard-departemen/DashboardDepartemen';
 function App() {
   return (
     <>
@@ -15,6 +15,7 @@ function App() {
       <Route path='/profile' Component={Profilekaryawan}/>
       <Route path='/laporan-karyawan' Component={LaporanKaryawan}/>
       <Route path='/dashboard' Component={dashboard}/>
+      <Route path='/dashboard-dept' Component={Dashboarddepartemen}/>
     </Routes>
     </>
   );
